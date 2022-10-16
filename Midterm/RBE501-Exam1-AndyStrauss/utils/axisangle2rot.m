5c6d7ae56a3934f2f41b0f019f97a7aa0872a118
Andrew Strauss <78166133+andrewstrauss18@users.noreply.github.com>
2022-10-16 12:55:46 -0400

    Delete Midterm directory

--- Midterm/RBE501-Exam1-AndyStrauss/utils/axisangle2rot.m
+++
@@ -1,4 +0,0 @@
-function R = axisangle2rot(omega,theta)
-    skewOmega = skew(omega);
-    R = eye(3) + sin(theta)*skewOmega + (1 - cos(theta))*skewOmega^2;
-end
--- Midterm/RBE501-Exam1-AndyStrauss/utils/fkine.m
+++
@@ -1,8 +0,0 @@
-function T = fkine(S,M,q)
-    N = size(S,2);
-    T = eye(4);
-    for i = 1:N
-        T = T*twist2ht(S(:,i),q(i));
-    end
-    T = T*M;
-end
